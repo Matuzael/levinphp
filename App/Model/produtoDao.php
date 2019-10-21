7a4c6e3a7e2c262753a4bbe39aabc552e9cae07b
matuzael <matuzaeldias@hotmail.com>
2019-10-21 11:33:16 -0300

    arquivos html transformados em php

--- App/Model/produtoDao.php
+++ App/Model/produtoDao.php
@@ -24,13 +24,43 @@ class ProdutoDAO{
 
     public function read(){
 
+        $sql = 'SELECT * FROM produtos';
+
+        $stmt = Conexao::getConn()->prepare($sql);
+
+        $stmt->execute();
+
+        if($stmt->rowCount()> 0):
+            //Tendo registros no banco, ele retorna um array para $resultados;
+            $resultado = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            return $resultado;
+        
+        else:
+            return [];
+        endif;
+
     }
 
     public function update(Produto $p){
+        $sql = 'UPDATE produtos set foto=?, nome=?, valor=? WHERE id = ? ';
 
+        $stmt = Conexao::getConn()->prepare($sql);
+        $stmt->bindValue(1, $p->getFoto());
+        $stmt->bindValue(2, $p->getNome());
+        $stmt->bindValue(3, $p->getValor());
+        $stmt->bindValue(4, $p->getId());
+
+        $stmt->execute();
     }
 
     public function delete($id){
+
+        $sql = 'DELETE FROM produtos WHERE id=?';
+
+        $stmt = Conexao::getConn()->prepare($sql);
+        $stmt->bindValue(1,$id);
+
+        $stmt->execute();
 
     }
 
