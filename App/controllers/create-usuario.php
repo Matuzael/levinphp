2ac429ed2cdd2e565e079d406d27d3424ffe767f
Matuzael dos Santos Dias <matuzael.dias@academico.ifrn.edu.br>
2019-10-29 10:43:38 -0300

    criando crud

--- App/controllers/create-usuario.php
+++ App/controllers/create-usuario.php
@@ -1,7 +1,23 @@
 <?php
+require_once '../vendor/autoload.php';
 
+$usuario = \App\Model\usuario();
 
-echo "<pre>" ,var_dump($_POST), "</pre>";
+$nome = $_POST['nome'];
+$sobrenme = $_POST['sobrenome'];
+$email = $_POST['email'];
+$endereco = $_POST['endereco'];
+$cidade = $_POST['cidade'];
+$estado = $_POST['estado'];
+$cep = $_POST['cep'];
+
+$usuario->setNome($nome);
+$usuario->setSobrenome($sobrenome);
+$usuario->setEmail($email);
+$usuario->setEndereco($endereco);
+$usuario->setCidade($cidade);
+$usuario->setEstado($estado);
+$usuario->setCep($cep);
 
 
 ?>
