3405ff2d3e2a741a0a66ce4d12cfb095b39188ac
Bernardo Vilela <53100991+1brnardo@users.noreply.github.com>
2019-12-04 16:11:40 -0300

    Update login.php

--- App/login.php
+++ App/login.php
@@ -1,46 +1,38 @@
-
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
-
-    <title>Signin Template for Bootstrap</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap//css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/signin.css" rel="stylesheet">
-
-
-  </head>
-
-  <body class="text-center">
-
-    <form class="form-signin" method="POST" action="controllers/loginVerification.php">
-    <a style="color:black" href="index.php">
-      <img class="mb-4" src="imgs/wine.png" alt="" width="72" height="72" >
-     <h5 class="logo"><strong>Le Vin</strong></h5></a>
-      <h1 class="h3 mb-3 font-weight-normal">Faça Login</h1>
-      <label for="inputEmail" class="sr-only">Email</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus  name="email">
-      <label for="inputPassword" class="sr-only">Senha</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required name="senha">
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <label >Não possui uma conta? <a href="cadastroUsuario.php" style="color:red"> Cadastre-se!</a></label>
-      </div>
-      <button class="btn btn-lg btn-danger btn-block" type="submit">Entrar</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-    </form>
-
-  </body>
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+      <title>Signin Template for Bootstrap</title>
+      <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+      <!-- Bootstrap core CSS -->
+      <link href="vendor/bootstrap//css/bootstrap.min.css" rel="stylesheet">
+      <!-- Custom styles for this template -->
+      <link href="css/signin.css" rel="stylesheet">
+   </head>
+  
+   <body class="text-center">
+      <form class="form-signin" method="POST" action="controllers/loginVerification.php">
+         <a style="color:black" href="index.php">
+            <img class="mb-4" src="imgs/wine.png" alt="" width="72" height="72" >
+            <h5 class="logo"><strong>Le Vin</strong></h5>
+         </a>
+         <h1 class="h3 mb-3 font-weight-normal">Faça Login</h1>
+         <label for="inputEmail" class="sr-only">Email</label>
+         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus  name="email">
+         <label for="inputPassword" class="sr-only">Senha</label>
+         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required name="senha">
+         <div class="checkbox mb-3">
+            <label>
+            <input type="checkbox" value="remember-me"> Remember me
+            </label>
+            <label >Não possui uma conta? <a href="cadastroUsuario.php" style="color:red"> Cadastre-se!</a></label>
+         </div>
+         <button class="btn btn-lg btn-danger btn-block" type="submit">Entrar</button>
+         <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+      </form>
+   </body>
 </html>
